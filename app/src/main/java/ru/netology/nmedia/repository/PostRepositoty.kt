7bd144d9e77bde5b671cb5ae9f4e23e978d656ec
Kirill Maximow <markizzzzka91@gmail.com>
2021-12-24 20:16:18 +0200

    rep added

---
+++ app/src/main/java/ru/netology/nmedia/repository/PostRepositoty.kt
@@ -0,0 +1,10 @@
+package ru.netology.nmedia
+
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.lifecycle.LiveData
+
+interface PostRepository {
+    fun get():LiveData<Post>
+    fun likeOrShare(textView: TextView, imageView: ImageView)
+}
