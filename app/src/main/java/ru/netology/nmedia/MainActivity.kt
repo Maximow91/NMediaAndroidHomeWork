7ce0f72f48c68aa46278e6cd15b68cb8fd8f3911
Kirill Maximow <markizzzzka91@gmail.com>
2021-12-27 20:30:54 +0200

    MainActivity modified

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -6,6 +6,18 @@ import ru.netology.nmedia.appViewModels.PostViewModel
 import ru.netology.nmedia.databinding.ActivityMainBinding
 import ru.netology.nmedia.repository.*
 
+fun forPrintPostFields(value:Int ):String {
+    var stringForPrint = ""
+    when(value) {
+        0 -> stringForPrint
+        in  1.. 1099 -> stringForPrint="$value"
+        in 1100 .. 9999 -> stringForPrint="${"%.1f".format(value.toDouble()/1000)}K"
+        in 10000 .. 99999 -> stringForPrint="${value/1000}K"
+        !in 0 .. 1000000 -> stringForPrint="${"%.1f".format(value.toDouble()/1000000)}M"
+    }
+    return stringForPrint
+}
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,22 +29,21 @@ class MainActivity : AppCompatActivity() {
 
         viewModel.data.observe(this){post ->
 
-            with(binding){
+            with(binding) {
                 author.text = post.author
                 published.text = post.published
                 content.text = post.content
                 likesValue.text = forPrintPostFields(post.likesValue)
                 sharedValue.text = forPrintPostFields(post.sharedValue)
 
-                if(post.likedByMe){
-                    likeButton.setImageResource(R.drawable.ic_favorite_24)
-                }
-        }
+                if (post.likedByMe) likeButton.setImageResource(R.drawable.ic_favorite_24)
+                else likeButton.setImageResource(R.drawable.ic_favorite_border_24)
+            }
             binding.likeButton.setOnClickListener {
-                viewModel.likeOrShare(binding.likesValue,binding.likeButton)
+                viewModel.like()
             }
             binding.shareButton.setOnClickListener{
-                viewModel.likeOrShare(binding.sharedValue,binding.shareButton)
+                viewModel.share()
             }
         }
     }
