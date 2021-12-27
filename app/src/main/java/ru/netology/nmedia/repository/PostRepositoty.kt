73ffda8c0c34e9aaeb0b5d02eb65c75d9c5b9563
Kirill Maximow <markizzzzka91@gmail.com>
2021-12-27 20:35:53 +0200

    PostRepository modified

--- app/src/main/java/ru/netology/nmedia/repository/PostRepositoty.kt
+++ app/src/main/java/ru/netology/nmedia/repository/PostRepositoty.kt
@@ -6,5 +6,6 @@ import androidx.lifecycle.LiveData
 
 interface PostRepository {
     fun get():LiveData<Post>
-    fun likeOrShare(textView: TextView, imageView: ImageView)
+    fun like()
+    fun share()
 }
